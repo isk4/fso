a3f95f39e36b84cf90ddb863298d629e0501dac9
isk4 <vgutierrezdiban@protonmail.com>
2025-12-30 16:19:46 -0300

    part1(anecdotes): implement random number based anecdote selection (ex 1.12)

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -11,12 +11,23 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
+
+  const getRandomNumber = (upperLimit) => {
+    return Math.floor(Math.random() * upperLimit);
+  };
    
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(getRandomNumber(anecdotes.length));
+
+  const handleClick = () => {
+    let newSelected = getRandomNumber(anecdotes.length);
+    while (newSelected === selected) newSelected = getRandomNumber(anecdotes.length);
+    setSelected(() => newSelected);
+  };
 
   return (
     <div>
-      {anecdotes[selected]}
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleClick}>Next anecdote</button>
     </div>
   )
 }
