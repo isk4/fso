6796c52c5d7bf2beba20b8d90d86614d584625c5
isk4 <vgutierrezdiban@protonmail.com>
2025-12-30 12:15:42 -0300

    part1: add feedback input and basic statistics to unicafe (ex 1.8 spec)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,9 +8,39 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <FeedbackInput setGood={setGood} setNeutral={setNeutral} setBad={setBad}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   );
 }
+
+const FeedbackInput = ({ setGood, setNeutral, setBad }) => {
+  return (
+    <div>
+      <h2>Give feedback</h2>
+      <div>
+        <button onClick={() => setGood((good) => good + 1)}>Good</button>
+        <button onClick={() => setNeutral((neutral) => neutral + 1)}>Neutral</button>
+        <button onClick={() => setBad((bad) => bad + 1)}>Bad</button>
+      </div>
+    </div>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const avg = (good * 1 + bad * -1) / all;
+  const positive = (good * 100) / all;
+  return (
+    <ul>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad: {bad}</li>
+      <li>All: {all}</li>
+      <li>Average: {Number.isNaN(avg) ? 0 : avg}</li>
+      <li>Positive: {Number.isNaN(positive) ? 0 : positive}%</li>
+    </ul>
+  );
+};
 
 export default App;
