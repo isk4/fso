f4063fac51c7b20006de03f9dcc1bee12ee9d752
isk4 <vgutierrezdiban@protonmail.com>
2025-12-30 15:27:59 -0300

    part1(unicafe): show message instead of statistics if no feedback present (ex 1.9)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -29,6 +29,9 @@ const FeedbackInput = ({ setGood, setNeutral, setBad }) => {
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
+  
+  if (all === 0) return <p>No feedback given.</p>;
+
   const avg = (good * 1 + bad * -1) / all;
   const positive = (good * 100) / all;
   return (
