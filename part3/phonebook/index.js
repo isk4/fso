c1901a5d61437c43711fa11106b4398dd7f3b1c3
isk4 <vgutierrezdiban@protonmail.com>
2026-01-06 15:37:07 -0300

    part3(phonebook): implement endpoint for deleting a single phonebook entry (ex 3.4)

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -38,6 +38,13 @@ app.get('/api/persons/:id', (request, response) => {
   person ? response.json(person) : response.status(404).end();
 });
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((p) => p.id !== id);
+
+  response.status(204).end();
+});
+
 app.get('/info', (request, response) => {
   response.send(`
     <p>Phonebook has info for ${persons.length} ${persons.length === 1 ? 'person' : 'people'}</p>
