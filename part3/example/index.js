428e03bf95b1c7b6e8b04878b0e7b26245010989
isk4 <vgutierrezdiban@protonmail.com>
2026-01-06 12:15:10 -0300

    part3(example): implement basic CRUD app with Express

--- part3/example/index.js
+++ part3/example/index.js
@@ -1,1 +1,79 @@
-console.log('Hello, world!');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+];
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello, world!</h1>');
+});
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes);
+});
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
+
+  note ? response.json(note) : response.status(404).end();
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = notes.reduce((max, note) => Math.max(max, note.id), 0);
+  return maxId + 1;
+};
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+  if (!body.content) return response.status(400).json({ error: 'content missing '});
+
+  const note = { 
+    content: body.content,
+    important: body.important || false,
+    id: generateId() 
+  };
+
+  notes.push(note);
+  response.json(note);
+});
+
+app.patch('/api/notes/:id', (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
+  const body = request.body;
+
+  if (body.content) note.content = body.content;
+  if (body.important) note.important = body.important;
+
+  response.json(note);
+});
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
