0f105e27cfc475e8c3de115e40a2d67752fc851b
isk4 <vgutierrezdiban@protonmail.com>
2026-01-06 22:09:35 -0300

    part3(phonebook): log POST request bodies with morgan (ex 3.8)

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,9 +3,12 @@ const morgan = require('morgan');
 
 const app = express();
 const PORT = 3001;
-
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('body', (request, response) => {
+  return request.method === 'POST' ? JSON.stringify(request.body) : '';
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
   { 
