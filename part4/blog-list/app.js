546b69e76bcc50c544629b605f0f2a4e13fb7a25
isk4 <vgutierrezdiban@protonmail.com>
2026-01-21 16:27:13 -0300

    part4(blog-list): refactor app into separate file (ex 4.2 spec)

---
+++ part4/blog-list/app.js
@@ -0,0 +1,16 @@
+require('./mongo');
+const express = require('express');
+const { requestLogger, unknownEndpointHandler, castErrorHandler } = require('./utils/middleware');
+const blogsRouter = require('./controllers/blogs');
+
+const app = express();
+
+app.use(express.json());
+app.use(requestLogger);
+
+app.use('/api/blogs', blogsRouter);
+
+app.use(unknownEndpointHandler);
+app.use(castErrorHandler);
+
+module.exports = app;
--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,33 +1,6 @@
-require('./mongo');
-const express = require('express');
-
+const app = require('./app');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-const { requestLogger, unknownEndpointHandler, castErrorHandler } = require('./utils/middleware');
-
-const Blog = require('./models/blog');
-
-const app = express();
-
-app.use(express.json());
-app.use(requestLogger);
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-app.use(unknownEndpointHandler);
-app.use(castErrorHandler);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}\n`);
