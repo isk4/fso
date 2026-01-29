d86b5f3589294d810342f5e7380169ac5a7eb413
isk4 <vgutierrezdiban@protonmail.com>
2026-01-29 16:07:59 -0300

    part4(blog_list): refactor test data as fixtures

--- part4/blog_list/tests/list_helper.test.js
+++ part4/blog_list/tests/list_helper.test.js
@@ -1,77 +1,11 @@
 const { test, describe } = require('node:test');
 const assert = require('node:assert/strict');
 const listHelper = require('../utils/list_helper');
-
-const listWithOneBlog = [
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-    likes: 5,
-    __v: 0
-  }
-];
-
-const blogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0
-  }
-];
-
-test('dummy returns one', () => {
-  const result = listHelper.dummy([]);
-  assert.strictEqual(result, 1);
-});
+const blogs = require('./fixtures/blogs.json');
 
 describe('total likes', () => {
   test('list with one blog returns likes of that blog', () => {
-    const result = listHelper.totalLikes(listWithOneBlog);
+    const result = listHelper.totalLikes([blogs[1]]);
     assert.strictEqual(result, 5);
   });
 
@@ -88,12 +22,12 @@ describe('total likes', () => {
 
 describe('favorite blog', () => {
   test('list with one blog returns the blog', () => {
-    const result = listHelper.favoriteBlog(listWithOneBlog);
+    const result = listHelper.favoriteBlog([blogs[1]]);
     assert.deepStrictEqual(result, {
       _id: '5a422aa71b54a676234d17f8',
       title: 'Go To Statement Considered Harmful',
       author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
       likes: 5,
       __v: 0
     });
@@ -124,7 +58,7 @@ describe('most blogs', () => {
   });
 
   test('list with one blog returns author of the blog', () => {
-    const result = listHelper.mostBlogs(listWithOneBlog);
+    const result = listHelper.mostBlogs([blogs[1]]);
     assert.deepStrictEqual(result, {
       author: 'Edsger W. Dijkstra',
       blogs: 1
@@ -147,7 +81,7 @@ describe('most likes', () => {
   });
 
   test('list with one blog returns likes of the blog', () => {
-    const result = listHelper.mostLikes(listWithOneBlog);
+    const result = listHelper.mostLikes([blogs[1]]);
     assert.deepStrictEqual(result, {
       author: 'Edsger W. Dijkstra',
       likes: 5
