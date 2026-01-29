84cb8182d8d75ac17cc85f7fc37ce447e8c0b0e8
isk4 <vgutierrezdiban@protonmail.com>
2026-01-29 12:02:03 -0300

    part4(blog_list): ensure connection to database before starting app

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,9 +1,17 @@
 const app = require('./app');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
+const { connectToMongo } = require('./mongo');
 
-require('./mongo');
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}\n`);
-});
+(async () => {
+  try {
+    await connectToMongo();
+    app.listen(config.PORT, () => {
+      logger.info(`Server running on port ${config.PORT}\n`);
+    });
+  } catch {
+    logger.error('\nStartup failed');
+    process.exit(1);
+  }
+})();
--- part4/blog_list/mongo.js
+++ part4/blog_list/mongo.js
@@ -5,12 +5,15 @@ const logger = require('./utils/logger');
 
 mongoose.set('strictQuery',false);
 
-(async () => {
+const connectToMongo = async () => {
   try {
     logger.info('Connecting to MongoDB...\n');
     await mongoose.connect(config.MONGODB_URI, { family: 4 });
     logger.info('Connected to MongoDB\n');
   } catch (e) {
     logger.error('Error connecting to MongoDB:', e.message);
+    throw e;
   }
-})();
+};
+
+module.exports = { connectToMongo };
