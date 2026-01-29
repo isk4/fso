4bff5630a3c454c6fe4d567e2fd0661ee975a30f
isk4 <vgutierrezdiban@protonmail.com>
2026-01-29 17:07:32 -0300

    part4(blog_list): test that /api/blogs endpoint returns the correct amount of blogs (ex 4.8)

--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -1,18 +1,22 @@
-const { test, describe, before, after, afterEach } = require('node:test');
+const { test, describe, beforeEach, before, afterEach, after } = require('node:test');
+const assert = require('node:assert/strict');
 const { connect, close, clear } = require('./db');
 const supertest = require('supertest');
 const app = require('../app');
+const Blog = require('../models/blog');
+const blogsFixture = require('./fixtures/blogs.json');
+
 const api = supertest(app);
 
 before(async () => { await connect(); });
+beforeEach(async () => await Blog.insertMany(blogsFixture));
 afterEach(async () => { await clear(); });
 after(async () => { await close(); });
 
 describe('API', () => {
-  test('blogs are returned as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
+  test('all blogs are returned as json', async () => {
+    const blogs = await api.get('/api/blogs');
+
+    assert.strictEqual(blogs.length, blogsFixture.legnth);
   });
 });
