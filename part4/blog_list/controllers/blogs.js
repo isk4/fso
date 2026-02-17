caabc5e2bbb20dd4c522f228cd1441b8e36cafb9
isk4 <vgutierrezdiban@protonmail.com>
2026-02-17 17:41:24 -0300

    part4(blog_list): implement user-blog association

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -1,20 +1,34 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({});
   response.json(blogs);
 });
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog({ ...request.body, likes: request.body?.likes ?? 0 });
+blogsRouter.post('/', async (request, response) => {
+  const user = await User.findOne({});
+
+  if (!user) {
+    return response.status(500).json({ error: 'no users available' });
+  }
+
+  const blog = new Blog({
+    ...request.body,
+    likes: request.body?.likes ?? 0,
+    user: user._id
+  });
+
   if (!blog.title || !blog.url) {
     return response.status(400).json({ error: 'title or url missing' });
   }
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  const savedBlog = await blog.save();
+  user.blogs.push(savedBlog._id);
+  await user.save();
+
+  response.status(201).json(savedBlog);
 });
 
 blogsRouter.patch('/:id', async (request, response) => {
